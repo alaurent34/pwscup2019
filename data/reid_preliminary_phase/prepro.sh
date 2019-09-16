efc4b7a7027ffe7caa8f394b770027a3545ecc8a
Antoine Laurent <antoine@famille-laurent.com>
2019-09-16 19:39:25 -0400

    data: add reid_phase

---
+++ data/reid_preliminary_phase/prepro.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+PWD="${PWD}"
+DIR="$1"
+
+REGION="../preliminary_phase/OrgData(Anony-Pre)_011/info_region.csv"
+
+for file in $DIR/*
+do
+    if [ -f $file ]; then
+        IFS='/' read -r -a path <<< "$file"
+        IFS='.' read -r -a filename <<< "${path[-1]}"
+        mkdir -p "prepro"
+        OUT_PATH="$PWD/prepro/prepro_${filename[-2]}/"
+
+        python preprocessing.py -i $file -o $OUT_PATH -r $REGION
+    fi
+done
