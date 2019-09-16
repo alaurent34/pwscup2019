f451ea7670d79833ab092079621e61e7fb203e5d
Antoine Laurent <antoine@famille-laurent.com>
2019-09-16 18:25:02 -0400

    data, prepro: sh script

--- data/preliminary_phase/prepro.sh
+++ data/preliminary_phase/prepro.sh
@@ -1,27 +1,16 @@
 #!/usr/bin/env bash
 PWD="${PWD}"
-REF="$1"
-ANO="$2"
+DIR="$1"
 
 REGION="../preliminary_phase/OrgData(Anony-Pre)_011/info_region.csv"
 
-for file in $REF/*
+for file in $DIR/*
 do
     if [ -f $file ]; then
         IFS='/' read -r -a path <<< "$file"
         IFS='.' read -r -a filename <<< "${path[-1]}"
-        OUT_PATH="$PWD/prepro_${filename[-2]}/"
-
-        python preprocessing.py -i $file -o $OUT_PATH -r $REGION
-    fi
-done
-
-for file in $ANO/*
-do
-    if [ -f $file ]; then
-        IFS='/' read -r -a path <<< "$file"
-        IFS='.' read -r -a filename <<< "${path[-1]}"
-        OUT_PATH="$PWD/prepro_${filename[-2]}/"
+        mkdir -p "prepro"
+        OUT_PATH="$PWD/prepro/prepro_${filename[-2]}/"
 
         python preprocessing.py -i $file -o $OUT_PATH -r $REGION
     fi
